2a0032921854bbf66007d392c1048010947a4876
Niko917 <kolbnickolay@yandex.ru>
2026-03-03 16:03:37 +0300

    configuration[cmake]

--- tests/queues/global/unit.cpp
+++ tests/queues/global/unit.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "../../../src/queues/global/global_queue.hpp"
+#include "queues/global/global_queue.hpp"
 
 // -------------------- Test prerequisites --------------------
 
