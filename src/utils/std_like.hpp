8a8f400eabd6388ad688460b752452892e9f71a4
Niko256 <kolbnickolay@yandex.ru>
2026-03-06 04:18:07 +0300

    configuration[Twist concurrency testing frameworkk]

--- src/utils/std_like.hpp
+++ src/utils/std_like.hpp
@@ -1,1 +1,28 @@
 #pragma once
+
+#ifdef WITH_TWIST
+
+#    include <atomic>
+#    include <twist/ed/std/atomic.hpp>
+#    include <twist/ed/std/condition_variable.hpp>
+#    include <twist/ed/std/mutex.hpp>
+#    include <twist/ed/std/thread.hpp>
+
+namenamespace wr {
+    nanamespace stdlike = ::twist::ed::std;
+}
+
+#else
+
+#    include <atomic>
+#    include <condition_variable>
+#    include <mutex>
+#    include <thread>
+
+namespace wr {
+
+namespace stdlike = ::std;
+
+}
+
+#endif
