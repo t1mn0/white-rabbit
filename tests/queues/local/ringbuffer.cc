04c81e38ca95ed14b5f3b383c3e4e92c6768c6c4
Niko917 <kolbnickolay@yandex.ru>
2026-03-06 13:55:07 +0300

    minor changes

--- tests/queues/local/ringbuffer.cc
+++ tests/queues/local/ringbuffer.cc
@@ -1,20 +1,31 @@
 #include "queues/local/ring_buffer.hpp"
 #include "vvv/list.hpp"
 #include <gtest/gtest.h>
+#include <iostream>
 
 struct InnerTask : public vvv::IntrusiveListNode<InnerTask> {
     int value;
 
     void run() noexcept {
-        std::cout << "Hello from task..." << std::endl;
+        std::cout << "...Hallo von der inneren aufgabe..." << std::endl;
     }
 };
 
 class RingBufferTests : public ::testing::Test {
   protected:
-    wr::queues::RingBuffer<InnerTask, 128> rbuff;
+    static constexpr size_t kCapacity = 128;
+
+    wr::queues::RingBuffer<InnerTask, kCapacity> rbuff;
 };
 
 TEST_F(RingBufferTests, CapacityCheck) {
-    EXPECT_EQ(rbuff.capacity(), 128);
+    EXPECT_EQ(rbuff.capacity(), kCapacity);
+}
+
+TEST_F(RingBufferTests, ToLocalIndexMapping) {
+    EXPECT_EQ(rbuff.to_local_index(0), 0);
+    EXPECT_EQ(rbuff.to_local_index(1), 1);
+    EXPECT_EQ(rbuff.to_local_index(127), 127);
+    EXPECT_EQ(rbuff.to_local_index(128), 0);
+    EXPECT_EQ(rbuff.to_local_index(257), 1);
 }
