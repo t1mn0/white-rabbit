00294368a5153954c7b049ecb94869bc982cb18f
Niko256 <kolbnickolay@yandex.ru>
2026-03-12 00:52:39 +0300

    [feature] : CMake presets + [configuration] : Twist configuration

--- tests/twist/fault_injection/ronaldo.cc
+++ tests/twist/fault_injection/ronaldo.cc
@@ -1,26 +1,34 @@
 #include <twist/ed/std/atomic.hpp>
 #include <twist/ed/std/thread.hpp>
+#include <twist/sim.hpp>
+
 
 #include <cassert>
 #include <iostream>
 
-void FaultInjectionSmoke() {
+void Ronaldo() {
     twist::ed::std::atomic<int> value{0};
 
     twist::ed::std::thread t([&] {
-        value.store(42, std::memory_order_release);
+        value.store(7, std::memory_order_release);
     });
 
     t.join();
 
-    assert(value.load(std::memory_order_acquire) == 42);
+    assert(value.load(std::memory_order_acquire) == 7);
 }
 
 int main() {
     std::cout << "[fault_injection] smoke test..." << std::endl;
 
+    twist::sim::sched::RandomScheduler scheduler{{}};
+
     for (int i = 0; i < 100; ++i) {
-        FaultInjectionSmoke();
+        twist::sim::Simulator sim{&scheduler};
+        auto result = sim.Run(Ronaldo);
+        assert(result.Ok());
+
+        scheduler.NextSchedule();
     }
 
     std::cout << "[fault_injection] PASSED!" << std::endl;
